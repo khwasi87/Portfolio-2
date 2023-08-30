1baf04c4d97d769ace25ecd9aeec104fd9c4084f
Wasim khan <khwasi.87@gmail.com>
2023-08-30 13:41:44 +0530

    updated style.css, index.html, style.js

--- js/style.js
+++ js/style.js
@@ -78,3 +78,22 @@ $(document).ready(function() {
 	  }
 	})
   })
+
+  $('#mod').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	dots:true,
+	navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
