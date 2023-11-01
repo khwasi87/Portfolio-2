85f473665c75798ca0a53dd74e203c7ce5d9ccdc
Wasim khan <khwasi.87@gmail.com>
2023-11-01 15:50:24 +0530

    added images and responsive css

--- js/style.js
+++ js/style.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
 	var owl = $('.owl-carousel');
 	owl.owlCarousel({
-	  	stagePadding: 75,
+	  	// stagePadding: 75,
 	 	margin: 15,
 		autoplay:true,
 		autoplayTimeout:5000,
@@ -124,21 +124,120 @@ $("#mod, #mod2, #mod3, #mod4, #mod5, #mod6, #mod7, #mod8, #mod9").each(function(
 
 /*============== exp year calculation =============*/
 
-var dob = $('#date').val();
-if(dob != ''){
-	var str=dob.split('-');    
-	var firstdate=new Date(str[0],str[1],str[2]);
-	var today = new Date();        
-	var dayDiff = Math.ceil(today.getTime() - firstdate.getTime()) / (1000 * 60 * 60 * 24 * 365);
-	var age = parseInt(dayDiff);
-	$('#age').html(age);
-}
-var dob = $('#date1').val();
-if(dob != ''){
-	var str=dob.split('-');    
-	var firstdate=new Date(str[0],str[1],str[2]);
-	var today = new Date();        
-	var dayDiff = Math.ceil(today.getTime() - firstdate.getTime()) / (1000 * 60 * 60 * 24 * 365);
-	var age = parseInt(dayDiff);
-	$('#age1').html(age+' Years - of experience');
-}
+/*========== time elapsed Js==============*/
+
+function calculateElapsedYears() {
+	var startDate = new Date(document.getElementById("start_date").value);
+	var endDate = new Date(document.getElementById("end_Date").value);
+   
+	var elapsedYears = endDate.getFullYear() - startDate.getFullYear();
+   
+	document.getElementById("elapsed_years").innerHTML = elapsedYears;
+
+	var startDate1 = new Date(document.getElementById("start_date1").value);
+	var endDate1 = new Date(document.getElementById("end_Date1").value);
+   
+	var elapsedYears1 = endDate1.getFullYear() - startDate1.getFullYear();
+   
+	document.getElementById("elapsed_years1").innerHTML = elapsedYears1;
+
+	var startDate1 = new Date(document.getElementById("start_date2").value);
+	var endDate1 = new Date(document.getElementById("end_Date2").value);
+   
+	var elapsedYears1 = endDate1.getFullYear() - startDate1.getFullYear();
+   
+	document.getElementById("elapsed_years2").innerHTML = elapsedYears1;
+   }
+   
+	   /*================ today date js================*/
+
+	   var date = new Date();
+   
+	   var day = date.getDate();
+	   var month = date.getMonth() + 1;
+	   var year = date.getFullYear();
+   
+	   if (month < 10) month = "0" + month;
+	   if (day < 10) day = "0" + day;
+   
+	   var today = year + "-" + month + "-" + day;
+   
+	   document.getElementById('end_Date').value = today;
+
+	   var date = new Date();
+   
+	   var day = date.getDate();
+	   var month = date.getMonth() + 1;
+	   var year = date.getFullYear();
+   
+	   if (month < 10) month = "0" + month;
+	   if (day < 10) day = "0" + day;
+   
+	   var today = year + "-" + month + "-" + day;
+   
+	   document.getElementById('end_Date2').value = today;
+
+	  /*========== mobile-js===============*/
+
+	  var date = new Date();
+   
+	  var day = date.getDate();
+	  var month = date.getMonth() + 1;
+	  var year = date.getFullYear();
+	  
+	  if (month < 10) month = "0" + month;
+	  if (day < 10) day = "0" + day;
+  
+	  var today = year + "-" + month + "-" + day;
+  
+	  document.getElementById('today1').value = today;
+
+	  $(document).ready(function() {
+		// Get the current date.
+		var today = new Date();
+   
+		// Get the date of birth from the input field.
+		var dob = new Date($('#start-date').val());
+   
+		// Calculate the age difference in years.
+		var age = today.getFullYear() - dob.getFullYear();
+   
+		// Display the age.
+		$('#age').html(age);
+	  });
+	  /*----------- exp 2nd --------------*/
+	  var date = new Date();
+   
+	  var day = date.getDate();
+	  var month = date.getMonth() + 1;
+	  var year = date.getFullYear();
+	  
+	  if (month < 10) month = "0" + month;
+	  if (day < 10) day = "0" + day;
+  
+	  var today = year + "-" + month + "-" + day;
+  
+	  document.getElementById('today2').value = today;
+
+	  $(document).ready(function() {
+		// Get the current date.
+		var today = new Date();
+   
+		// Get the date of birth from the input field.
+		var dob = new Date($('#start-date1').val());
+   
+		// Calculate the age difference in years.
+		var age = today.getFullYear() - dob.getFullYear();
+   
+		// Display the age.
+		$('#age1').html(age);
+	  });
+
+	  /*============ mobile js ends===========*/
+
+	   /*============ button disappear js============*/
+
+	   function disappear() {
+		document.getElementById("myButton").style.display = "none";
+	  }
+	  document.getElementById("myButton").addEventListener("click", disappear);
